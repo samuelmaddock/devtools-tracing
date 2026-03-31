a9dfdf6113eeb11492e27c95ddc29f0d0441d7b0
Samuel Maddock <smaddock@slack-corp.com>
2026-03-30 21:30:37 -0400

    fix: register experiments for sourcemaps

--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,7 @@
 import { DevToolsLocale } from '../lib/front_end/core/i18n/DevToolsLocale.js';
 import { registerLocaleDataForTest } from '../lib/front_end/core/i18n/i18nImpl.js';
+import { ExperimentName } from '../lib/front_end/core/root/ExperimentNames.js';
+import { experiments } from '../lib/front_end/core/root/Runtime.js';
 
 export function initDevToolsTracing() {
   registerLocaleDataForTest('en-US', {});
@@ -11,4 +13,16 @@ export function initDevToolsTracing() {
     lookupClosestDevToolsLocale: identity,
   };
   DevToolsLocale.instance({ create: true, data });
+
+  // Register experiments so isEnabled() checks don't throw.
+  for (const name of Object.values(ExperimentName)) {
+    if (name === ExperimentName.ALL) {
+      continue;
+    }
+    try {
+      experiments.register(name, name);
+    } catch {
+      // Already registered.
+    }
+  }
 }
